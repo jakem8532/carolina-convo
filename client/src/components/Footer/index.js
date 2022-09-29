9fe8139658805f60db57d3e3a2f89392d652e37e
cmon-theres-gotta-be-a-name-available <ben.ridgeway9@gmail.com>
2022-09-29 17:38:57 -0400

    ConvoList, Footer, Header

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            Contributors:
+            <div> 
+                <a href="">Forrest Ridgeway's Portfolio</a>
+            </div>
+            <div>
+                <a href="">_____'s Portfolio</a>
+            </div>
+            <div>
+                <a href="">_____'s Portfolio</a>
+            </div>
+            <div>
+                <a href="">_____'s Portfolio</a>
+            </div>
+            <div>
+                <a href="">_____'s Portfolio</a>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
