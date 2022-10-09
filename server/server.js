d90e543c305f061f5e07dd04ecf927c743c0309d
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-10-08 21:07:17 -0400

    heroku push

--- server/server.js
+++ server/server.js
@@ -24,13 +24,13 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
 };
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build/index.html")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build/index.html")));
+}
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
