92a3801a8e2f80e6584d8bb3c2f3150e8799e361
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-10-08 23:26:04 -0400

    added pages

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,19 +5,16 @@ const Footer = () => {
         <footer>
             Contributors:
             <div> 
-                <a href="">Forrest Ridgeway's Portfolio</a>
+                <a href="https://github/com.cmon-theres-gotta-be-a-name-available">Forrest Ridgeway's Portfolio</a>
             </div>
             <div>
-                <a href="">_____'s Portfolio</a>
+                <a href="https://github.com/jakem8532">Jake Mendez's Portfolio</a>
             </div>
             <div>
-                <a href="">_____'s Portfolio</a>
+                <a href="https://github.com/BradsW90">Bradley Woodle's Portfolio</a>
             </div>
             <div>
-                <a href="">_____'s Portfolio</a>
-            </div>
-            <div>
-                <a href="">_____'s Portfolio</a>
+                <a href="https://github.com/yo-Pack">Zack Pawloski's Portfolio</a>
             </div>
         </footer>
     )
