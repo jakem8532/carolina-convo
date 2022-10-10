85966546f744232e8fd577f3aed68413273ad4e2
Bradley <bswj90@gmail.com>
2022-10-09 21:41:33 -0400

    fixed site non rendering

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,40 @@
-import React from 'react';
-import ConvoList from '../components/ConvoList';
-import PostForm from '../components/PostForm';
+import React from "react";
+import ConvoList from "../components/ConvoList";
+import PostForm from "../components/PostForm";
 
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { QUERY_CHATS, QUERY_ME_BASIC } from '../utils/queries';
-import FriendList from '../components/FriendList';
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_CHATS, QUERY_ME_BASIC } from "../utils/queries";
+import FriendList from "../components/FriendList";
 
 const Home = () => {
-    const { loading, data } = useQuery(QUERY_CHATS);
-    const { data: userData } = useQuery(QUERY_ME_BASIC);
-    const convos = data?.convos || [];
+  const { loading, data } = useQuery(QUERY_CHATS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const convos = data?.convos || [];
 
-    const loggedIn = Auth.loggedIn();
+  const loggedIn = Auth.loggedIn();
 
-    return(
-        <main>
-            <div className={`convo`}>
-              {loading ? (
-                <div>Loading</div>
-              ) : (
-                <ConvoList convos={convos} title='Your conversations'/>
-              )}
-            </div>
-            <div>
-                {loggedIn && (
-                    <div>
-                        <PostForm/>
-                    </div>
-                )}
-            </div>
-            {loggedIn && userData ? (
-              <div className='friend-list'>
-                  <FriendList
-                    username={userData.me.username}
-                    friendCount={userData.me.friendCount}
-                    friends={userData.me.friends}
-                  />
-              </div>
-            ) : null }
-        </main>
-    );
+  return (
+    <main>
+      <div className={`convo`}>
+        {loading ? (
+          <div>Loading</div>
+        ) : (
+          <ConvoList convos={convos} title="Your conversations" />
+        )}
+      </div>
+      <div>{loggedIn && <div></div>}</div>
+      {loggedIn && userData ? (
+        <div className="friend-list">
+          <FriendList
+            username={userData.me.username}
+            friendCount={userData.me.friendCount}
+            friends={userData.me.friends}
+          />
+        </div>
+      ) : null}
+    </main>
+  );
 };
 
 export default Home;
